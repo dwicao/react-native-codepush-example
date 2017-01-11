2c85458718e05edc2f66ee6e5c00b3e8bf4a019f
dwicao <lutfiandwica@gmail.com>
2017-01-11 13:56:47 +0700

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import codePush from "react-native-code-push";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class coba2 extends Component {
+  componentDidMount() {
+    codePush.sync({installMode: codePush.InstallMode.IMMEDIATE});
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Selamat Datang di React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          Ini adalah contoh RN + CodePush
+        </Text>
+        <Text style={styles.instructions}>
+          Ini nanti akan saya ubah warna backgroundnya jadi hitam
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('coba2', () => codePush(coba2));
